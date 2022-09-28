29c9cc0cc074e1212942e9256c8fc504c1d9c622
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-09-28 14:09:50 -0600

    Article page GET works, needs tuning once front end is working.

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,4 @@
-// WEBSITE CONTROLLER PAGE.
+// POST CONTROLLER 
 
 let mongoose = require('mongoose');
 
@@ -10,6 +10,21 @@ exports.home_page_get = function(req, res, next) {
 
     Post.find()
     .populate('author', 'username')
+
+    .exec(function (err, result) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(result);
+    });
+}
+
+// GET request for single article page.
+exports.article_page_get = function (req, res, next) {
+    Post.findById( req.params.id )
+    .populate('author', 'username')
+
     .exec(function (err, result) {
         if (err) {
             return next(err);
--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 // Controller module import.
-let controller = require("../controllers/api");
+let postController = require("../controllers/postController");
 
-/* GET home page. */
-router.get('/', controller.home_page_get);
+// Display all posts on GET.
+router.get('/posts', postController.home_page_get);
+
+// Display single post details and comments on GET.
+router.get('/post/:id', postController.article_page_get);
 
 module.exports = router;
+
+// api/post/63320a91595e80131ae053f2
