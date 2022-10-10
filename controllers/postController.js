fb2dd4158435a6fa3f23b8478f3e03db93711bfb
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-10-10 16:38:28 -0600

    Update_post method added and working.

--- controllers/postController.js
+++ controllers/postController.js
@@ -33,6 +33,7 @@ exports.get_single_post = function(req, res, next) {
 
 // POST - Create post.
 exports.post_create_post = [
+
     body('title', "Title is required")
     .trim()
     .isLength({ min: 1}),
@@ -47,10 +48,9 @@ exports.post_create_post = [
 
     // Process data.
     (req, res, next) => {
-        
+
         // Used for returning data to form if errors in client.
         const errors = validationResult(req);
-        console.log(errors);
 
         if (!errors.isEmpty()) {
             return res.status(400).send({ errors : errors.array() });
@@ -72,15 +72,62 @@ exports.post_create_post = [
     }
 ]
 
+// POST - Update selected post
+exports.update_post = [
+    
+    body('title', "Title is required")
+    .trim()
+    .isLength({ min: 1}),
+
+    body('author', "Author name is required")
+    .trim()
+    .isLength({ min: 1}),
+
+    body('text', "Post text is required")
+    .trim()
+    .isLength({ min: 1}),
+
+    // Process data.
+    (req, res, next) => {
+        
+        // Used for returning data to form if errors in client.
+        const errors = validationResult(req);
+        console.log(req.body);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).send({ errors : errors.array() });
+        }
+
+        Post.findByIdAndUpdate(req.params.id, 
+            {
+                "title" : req.body.title,
+                "author" : req.body.author,
+                "text" : req.body.text,
+            },
+
+            function(err, result) {
+                if (err) { 
+                    return res.json(err); 
+                } else {
+                    return res.json(result);
+                }
+            }
+        )
+
+    }
+]
+
 // DELETE - Delete selected post.
 exports.delete_post = function(req, res, next) {
+
     Post.deleteOne({ _id : req.params.id },
         function(err, result) {
             if (err) {
-                return next(err);
+                return res.json(err);
             }
-
-            return res.redirect('/');
+            return res.json({ 
+                message: "Post has been deleted." 
+            })
         }
     )
 }
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -13,7 +13,10 @@ router.get('/post/:id', postController.get_single_post);
 // Create new post on POST from '/create_post' page/
 router.post('/post/create_post', postController.post_create_post);
 
+// Update selected post on POST from 
+router.post('/post/:id', postController.update_post);
+
 // Delete post 
-router.delete('/post/:id', postController.delete_post);
+router.delete('/post/:id/delete', postController.delete_post);
 
 module.exports = router;
